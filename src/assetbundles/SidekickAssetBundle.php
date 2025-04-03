012e6a18290e6b7ca43bf3bf1f8140f581fc887e
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2025-04-03 10:15:08 -0700

    Added copyright to all PHP pages

--- src/assetbundles/SidekickAssetBundle.php
+++ src/assetbundles/SidekickAssetBundle.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Sidekick plugin for Craft CMS
+ *
+ * Your AI companion for rapid Craft CMS development.
+ *
+ * @author    Double Secret Agency
+ * @link      https://plugins.doublesecretagency.com/
+ * @copyright Copyright (c) 2025 Double Secret Agency
+ */
 
 namespace doublesecretagency\sidekick\assetbundles;
 
